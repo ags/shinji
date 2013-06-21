faca0b6945cee2d9f279e64b36ed9d7138aa7594
Alex Smith <alex.geoffrey.smith@gmail.com>
2013-06-21 22:04:42 +1000

    TransactionPayload spec.

--- lib/shinji/transaction_payload.rb
+++ lib/shinji/transaction_payload.rb
@@ -2,16 +2,16 @@ module Shinji
   class TransactionPayload < Struct.new(:event, :sql_events, :view_events)
     def to_h
       {
-        path: event.payload[:path],
-        status: event.payload[:status],
-        started_at: event.time.to_f,
-        ended_at: event.end.to_f,
-        db_runtime: event.payload[:db_runtime],
+        path:         event.payload[:path],
+        status:       event.payload[:status],
+        started_at:   event.time.to_f,
+        ended_at:     event.end.to_f,
+        db_runtime:   event.payload[:db_runtime],
         view_runtime: event.payload[:view_runtime],
-        duration: event.duration,
-        source: source,
-        sql_events: sql_events_hash,
-        view_events: view_events_hash
+        duration:     event.duration,
+        source:       source,
+        sql_events:   sql_events_hash,
+        view_events:  view_events_hash
       }
     end
 
@@ -19,21 +19,21 @@ module Shinji
 
     def source
       {
-        controller: event.payload[:controller],
-        action: event.payload[:action],
-        format_type: event.payload[:format],
-        method_name: event.payload[:method]
+        controller:   event.payload[:controller],
+        action:       event.payload[:action],
+        format_type:  event.payload[:format],
+        method_name:  event.payload[:method]
       }
     end
 
     def sql_events_hash
       sql_events.map { |sql_event|
         {
-          sql: sql_event.payload[:sql],
-          name: sql_event.payload[:name],
+          sql:        sql_event.payload[:sql],
+          name:       sql_event.payload[:name],
           started_at: sql_event.time.to_f,
-          ended_at: sql_event.end.to_f,
-          duration: sql_event.duration
+          ended_at:   sql_event.end.to_f,
+          duration:   sql_event.duration
         }
       }
     end
@@ -41,12 +41,16 @@ module Shinji
     def view_events_hash
       view_events.map { |view_event|
         {
-          identifier: view_event.payload.fetch(:identifier).sub(Rails.root.to_s, ''),
+          identifier: strip_rails_root(view_event.payload.fetch(:identifier)),
           started_at: view_event.time.to_f,
-          ended_at: view_event.end.to_f,
-          duration: view_event.duration
+          ended_at:   view_event.end.to_f,
+          duration:   view_event.duration
         }
       }
+    end
+
+    def strip_rails_root(target)
+      target.sub(Rails.root.to_s, '')
     end
   end
 end
--- spec/shinji/transaction_payload_spec.rb
+++ spec/shinji/transaction_payload_spec.rb
@@ -1,5 +1,89 @@
 require "spec_helper"
 
 describe Shinji::TransactionPayload do
-  pending
+  describe "#to_h" do
+    let(:event) {
+      stub(:event,
+        duration: 5.79,
+        time:     Time.at(1371815334.955212),
+        end:      Time.at(1371815335.955212),
+        payload: {
+          path:         "/posts/new",
+          status:       200,
+          db_runtime:   1.23,
+          view_runtime: 4.56,
+          controller:   "PostsController",
+          action:       "new",
+          format:       "*/*",
+          method:       "GET"
+        }
+      )
+    }
+
+    let(:sql_events) { [
+      stub(:sql_event,
+        time:     Time.at(1371815336.955212),
+        end:      Time.at(1371815337.955212),
+        duration: 1,
+        payload: {
+          sql: "SELECT * FROM posts",
+          name: "Post Load"
+        }
+      )
+    ] }
+
+    let(:view_events) { [
+      stub(:view_event,
+        time:     Time.at(1371815338.955212),
+        end:      Time.at(1371815339.955212),
+        duration: 1,
+        payload: {
+          identifier: "/home/bob/foo/app/views/foobar.html"
+        }
+      )
+    ] }
+
+    before do
+      Rails.stub(:root) { "/home/bob/foo" }
+    end
+
+    it "returns a hash representation of event data" do
+      payload = Shinji::TransactionPayload.new(event, sql_events, view_events)
+
+      expect(payload.to_h).to eq(
+        {
+          path:         "/posts/new",
+          status:       200,
+          started_at:   1371815334.955212,
+          ended_at:     1371815335.955212,
+          db_runtime:   1.23,
+          view_runtime: 4.56,
+          duration:     5.79,
+          source: {
+            controller:   "PostsController",
+            action:       "new",
+            format_type:  "*/*",
+            method_name:  "GET"
+          },
+          sql_events: [
+            {
+              started_at: 1371815336.955212,
+              ended_at:   1371815337.955212,
+              duration:   1,
+              name:       "Post Load",
+              sql:        "SELECT * FROM posts"
+            }
+          ],
+          view_events: [
+            {
+              started_at: 1371815338.955212,
+              ended_at:   1371815339.955212,
+              duration:   1,
+              identifier: "/app/views/foobar.html"
+            }
+          ]
+        }
+      )
+    end
+  end
 end
